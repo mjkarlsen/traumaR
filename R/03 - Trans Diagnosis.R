71e64586b517a85ca0b82cdbe40210fcc0a6a296
ultramattyice <matthew.j.carlson00@gmail.com>
2020-05-03 16:10:08 -0500

    Added Procedures and Diagnosis

--- R/03 - Trans Diagnosis.R
+++ R/03 - Trans Diagnosis.R
@@ -1,25 +1,32 @@
-#' Diagnosis Data
+#' Create Normalized Diagnosis Data
 #'
 #' @param ptos_df
 #'
 #' @return
 #'
 #' @examples
-diagnosis_data <- function(.data) {
-
-  diag_df <- .data %>%
-    select.(id, ref_ar_d_a, ref_ar_t_a, starts_with.('icd9_')) %>%
-    pivot_longer.(cols = c(-id, ref_ar_d_a, ref_ar_t_a),
-                  values_to = "code",
-                  values_drop_na = TRUE
+create_diag_df <- function(.data) {
+  .data <- .data %>%
+    select.(id,
+            ref_ar_d_a,
+            ref_ar_t_a,
+            starts_with.('icd9_')) %>%
+    pivot_longer.(
+      cols = -c(id, ref_ar_d_a, ref_ar_t_a),
+      values_to = "code",
+      values_drop_na = TRUE
     ) %>%
     separate.(name, into = c("type", "serial"), sep = "_") %>%
-    mutate.(code = as.double(code)) %>%
+    mutate.(diag_code = as.double(code),
+            loc_desc = "Diagnosis") %>%
     left_join.(diagnosis_data, by = "diag_code") %>%
-    rename.(date = ref_ar_dd,
-            time = ref_ar_t_a,
-            code_desc = shot_desc)
-    select(id, date, time, code, code_desc)
+    rename.(
+      date = ref_ar_d_a,
+      time = ref_ar_t_a,
+      code_cd = diag_code,
+      code_desc = short_desc
+    ) %>%
+    select.(id, date, time, loc_desc,  code_cd, code_desc)
 
   return(.data)
 }
---
+++ R/03 - Trans Procedure.R
@@ -0,0 +1,43 @@
+#' Create Normalized Procedure Data
+#'
+#' @param .data
+#'
+#' @return
+#'
+#' @examples
+create_proc_df <- function(.data) {
+  .data <- .data %>%
+    select.(id, starts_with.('proc_')) %>%
+    select.(id,
+            ends_with.('da'),
+            ends_with.('ta'),
+            ends_with.('pr'),
+            ends_with.('lc')) %>%
+    pivot_longer.(cols = -id,
+                  values_to = "values",
+                  values_drop_na = TRUE) %>%
+    filter.(values != "NA") %>%
+    separate.(name,
+              into = c("type", "serial", "desc"),
+              sep = "_") %>%
+    pivot_wider.(
+      .,
+      names_from = desc,
+      values_from = values,
+      values_fn = list
+    ) %>%
+    mutate.(
+      date = mdy(da),
+      time = as.character(ta),
+      proc_code = as.character(pr),
+      loc_desc = proc_lc(as.character(lc)),
+      proc_code = as.double(proc_code)
+    ) %>%
+    left_join.(icd_data, by = "proc_code") %>%
+    rename.(code_cd = proc_code,
+            code_desc = short_desc) %>%
+    select.(id, date, time, loc_desc, code_cd, code_desc)
+
+
+  return(.data)
+}
