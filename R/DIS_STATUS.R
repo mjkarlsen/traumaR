4f32bf041f1bfd0dcb2d4e99dfddcad3ee2d8293
ultramattyice <matthew.j.carlson00@gmail.com>
2020-05-11 11:04:08 -0500

    Added dis_status to functions

--- R/DIS_STATUS.R
+++ R/DIS_STATUS.R
@@ -5,7 +5,6 @@
 #' @return It translations of the code into human friendly values.
 #' @export
 #'
-#' @examples
 dis_status <- function(col) {
   col_value <-  case.(col == 6 ,  'Alive' ,
                       col == 7 ,  'D»ead' ,
