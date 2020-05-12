a23dbe58b2a454629bf2de831a5c6ee659f01aaf
ultramattyice <matthew.j.carlson00@gmail.com>
2020-05-12 14:40:30 -0500

    Using library icd.data to pull all of the diagnosis data - original list was missing results

--- data-raw/diagnosis_data.R
+++ data-raw/diagnosis_data.R
@@ -1,5 +1,16 @@
 ## code to prepare `diagnosis_data` dataset goes here
 
-diagnosis_data <- arrow::read_csv_arrow("E:/Northwestern/12 - Capstone/Data/diagnosis_data.csv")
+library(icd.data)
+
+
+# diagnosis_data <- arrow::read_csv_arrow("E:/Northwestern/12 - Capstone/Data/diagnosis_data.csv")
+
+
+diagnosis_data <-
+  icd9cm_billable$`32` %>%
+  select.(code, short_desc, long_desc) %>%
+  mutate.(diag_code = as.double(code)/100) %>%
+  select.(diag_code, everything.()) %>%
+  drop_na.()
 
 usethis::use_data(diagnosis_data, overwrite = TRUE)
