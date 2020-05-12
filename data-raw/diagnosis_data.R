42db4e7a3bbc949f4386f2e94c71329f0b06b00d
ultramattyice <matthew.j.carlson00@gmail.com>
2020-05-12 16:00:47 -0500

    Additional clean up on codes when converting char to double

--- data-raw/diagnosis_data.R
+++ data-raw/diagnosis_data.R
@@ -5,12 +5,16 @@ library(icd.data)
 
 # diagnosis_data <- arrow::read_csv_arrow("E:/Northwestern/12 - Capstone/Data/diagnosis_data.csv")
 
-
 diagnosis_data <-
   icd9cm_billable$`32` %>%
   select.(code, short_desc, long_desc) %>%
-  mutate.(diag_code = as.double(code)/10) %>%
-  select.(diag_code, everything.()) %>%
+  mutate.(diag_leng = str_length(code),
+          code = as.double(code),
+          diag_code = ifelse.(diag_leng == 5,code/100, code/10),
+          new_code = as.double(code)) %>%
+  select.(diag_code,code, long_desc, short_desc) %>%
   drop_na.()
+
+# diagnosis_data %>% View()
 
 usethis::use_data(diagnosis_data, overwrite = TRUE)
