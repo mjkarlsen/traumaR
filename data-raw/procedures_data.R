255791ccc0881f7313f636c85d126fd989acb8d6
ultramattyice <matthew.j.carlson00@gmail.com>
2020-05-12 14:35:28 -0500

    Added Chapter and Subchapters to procedures

--- data-raw/procedures_data.R
+++ data-raw/procedures_data.R
@@ -2,4 +2,9 @@
 
 procedures_data <- arrow::read_csv_arrow("E:/Northwestern/12 - Capstone/Data/procedures_data.csv")
 
+procedures_data <- procedures_data %>%
+  mutate.(chapter = traumaR::procedure_lvl_1(proc_code),
+          subchapter = traumaR::procedure_lvl_2(proc_code)) %>%
+  select.(proc_code, chapter, subchapter, procedure_desc, short_desc)
+
 usethis::use_data(procedures_data, overwrite = TRUE)
