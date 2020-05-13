b30b4cfde37cc5445532609d708f86616f219be2
ultramattyice <matthew.j.carlson00@gmail.com>
2020-05-13 05:36:07 -0500

    Added chapter and subchapter for procedures and diagnosis.
    Added level 1 for injury using e_code

--- data-raw/diagnosis_data.R
+++ data-raw/diagnosis_data.R
@@ -13,8 +13,25 @@ diagnosis_data <-
           diag_code = ifelse.(diag_leng == 5,code/100, code/10),
           new_code = as.double(code)) %>%
   select.(diag_code,code, long_desc, short_desc) %>%
+  mutate.(chapter = traumaR::diagnosis_lvl_1(proc_code),
+          subchapter = traumaR::diagnosis_lvl_2(proc_code)) %>%
   drop_na.()
 
 # diagnosis_data %>% View()
+
+########################################################
+# For Case Statement Diagnosis Level 1
+########################################################
+
+# test_df <- icd.data::icd9_sub_chapters %>%
+#   enframe() %>%
+#   mutate.(value = as.character(value),
+#           ve_test = stringr::str_detect(value, '\"V|\"E'))  %>%
+#   filter.(ve_test != T) %>%
+#   rename.(subchapter = name) %>%
+#   separate.(value, into = c('start', 'end'), sep = ",") %>%
+#   mutate.(start =  as.double(unlist(regmatches(start, gregexpr("[[:digit:]]+", start)))),
+#           end =  as.double(unlist(regmatches(end, gregexpr("[[:digit:]]+", end))))) %>%
+#   select.(-ve_test)
 
 usethis::use_data(diagnosis_data, overwrite = TRUE)
